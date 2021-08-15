4c661ec77829cbb92763c7324daad39105e72180
hovanvydut <hovanvydut@gmail.com>
2021-08-15 20:29:08 +0700

    remove bin folder

--- PBL3Servlet/bin/target/PBL3Servlet-1.0-SNAPSHOT/static/js/setCurrentDateForDateTimePicker.js
+++
@@ -1,8 +0,0 @@
-let dateObj = new Date();
-let month = dateObj.getUTCMonth() + 1; //months from 1-12
-let day = dateObj.getUTCDate();
-let year = dateObj.getUTCFullYear();
-
-newdate = year + "-" + (month.length > 1 ? month : '0' + month) + "-" + day;
-document.getElementById("fromDate").setAttribute("value", newdate);
-document.getElementById("toDate").setAttribute("value", newdate);
